9099506114fbb94923fbf3134d4f1ba99610eeb8
Tim Čas <darkuranium@gmail.com>
2011-06-02 18:26:29 +0200

    Added sgmModuleTick support.

--- include/siege/backend/module.h
+++ include/siege/backend/module.h
@@ -24,6 +24,8 @@ extern "C"
     SGuint SG_EXPORT sgmModuleInit(SGModuleInfo** minfo);
     SGuint SG_EXPORT sgmModuleExit(SGModuleInfo* minfo);
     //SGuint SG_EXPORT sgmModuleFree(void* data);
+    
+    SGuint SG_EXPORT sgmModuleTick(SGulong tick);
 
     SGuint SG_EXPORT sgmModuleMatch(SGModuleInfo** minfos, SGuint numinfos, SGbool* ok);
 #ifdef __cplusplus
