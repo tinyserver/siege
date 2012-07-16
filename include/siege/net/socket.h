1a5728cf403920c32794b4a94126416b089714a5
Bernard Helyer <b.helyer@gmail.com>
2012-07-16 20:12:11 +1200

    Socket Documentation!

--- include/siege/net/socket.h
+++ include/siege/net/socket.h
@@ -26,25 +26,69 @@ typedef struct SGSocket
 {
 } SGSocket;
 
+/// Create a socket. Defaults to blocking.
 SGSocket* SG_EXPORT sgSocketCreate(SGenum addressType, SGenum socketType);
+/// Release the memory for a socket. Calls sgSocketClose if needed.
 void SG_EXPORT sgSocketDestroy(SGSocket* socket);
 
+/// Set the blocking status of the given socket.
 void SG_EXPORT sgSocketSetBlocking(SGSocket* socket, SGbool blocking);
+/// Retrieve the blocking status of the given socket.
 SGbool SG_EXPORT sgSocketGetBlocking(SGSocket* socket);
-
+/// Returns: true if this is a valid, living socket.
 SGbool SG_EXPORT sgSocketIsAlive(SGSocket* socket);
-
+/// Associate an address with this socket.
 SGvoid SG_EXPORT sgSocketBind(SGSocket* socket, SGAddress* address);
+/// Connect to an address. If socket is blocking, this waits until the connection is established.
 SGvoid SG_EXPORT sgSocketConnect(SGSocket* socket, SGAddress* address);
+/**
+ * Listen for an incoming connection.
+ * An address must have been associated with the socket with sgSocketBind.
+ * Params:
+ *   socket = the socket to listen on.
+ *   backlog = how many connections to queue until they're accepted.
+ */
 SGvoid SG_EXPORT sgSocketListen(SGSocket* socket, SGint backlog);
+/**
+ * Accept an incoming connection.
+ *
+ * Returns: the accepted socket, or NULL if unable to accept.
+ */
 SGSocket* SG_EXPORT sgSocketAccept(SGSocket* socket);
+/// Disable sends and recvs from this socket.
 SGvoid SG_EXPORT sgSocketShutdown(SGSocket* socket, SGenum how);
+/// Drop connections from this socket. Calling sgSocketShutdown is recommended.
 SGvoid SG_EXPORT sgSocketClose(SGSocket* socket);
 
+/**
+ * Sends data out on the given socket.
+ *
+ * This call blocks if the socket is set to blocking mode.
+ *
+ * Params:
+ *   socket = the socket to send data from.
+ *   buf = the data to send.
+ *   buflen = the length of the data, in bytes, in buf.
+ * Returns: the actual amount of data sent.
+ *          Less than zero on error.
+ */
 SGint SG_EXPORT sgSocketSend(SGSocket* socket, SGubyte* buf, SGint buflen);
-SGint SG_EXPORT sgSocketRecv(SGSocket* socket, SGubyte* buf, SGint maxbuf, SGint* buflen)
 
-SGvoid SG_EXPORT sgSocketKeepAlive(SGSocket* socket, SGint time, SGint interval);
+/**
+ * Receive data from the given socket.
+ *
+ * This call blocks if the socket is set to blocking mode.
+ *
+ * Params:
+ *   socket = the socket to get data from.
+ *   buf = a buffer to store received data.
+ *   maxbuf = the total length of the buf buffer.
+ *
+ * Returns: the number of bytes received. 
+ *          Zero if the remote closed the connection. 
+ *          Less than zero on an error.
+ */
+SGint SG_EXPORT sgSocketRecv(SGSocket* socket, SGubyte* buf, SGint maxbuf)
 
 #ifdef __cplusplus
 }
